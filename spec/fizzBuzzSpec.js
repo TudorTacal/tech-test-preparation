264e86f3958203444b7614587484a026cb0097b1
TudorTacal <tudortacal@gmail.com>
2017-02-15 15:41:40 +0000

    Test for divisibility with 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -14,10 +14,14 @@ describe("FizzBuzz", function(){
     it("returns true if is divisible by three", function (){
       expect(fizzBuzz.isDivisibleBy(3)).toBe(true)
     });
-
     it("return false if it is not divisible by three", function(){
       expect(fizzBuzz.isDivisibleBy(4)).toBe(false)
     });
+    it("return true if it's divisible by 5", function () {
+      expect(fizzBuzz.isDivisibleBy(5)).toBe(true)
+    })
+
+
   });
 
 });
